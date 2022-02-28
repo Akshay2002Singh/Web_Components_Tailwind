4d8dbc4d26a539afce821419daf564ce95e40b0f
Akshay2002Singh <06akshay2002@gmail.com>
2022-02-28 16:05:25 +0530

    components added

---
+++ card.js
@@ -0,0 +1,9 @@
+const auto_suggestion_card = (<div className="w-60 m-2 p-2 border-2 border-solid rounded-lg border-black flex-col">
+<div className="text-center text-2xl font-bold italic font-serif">Akshay Singh</div>
+<div className="w-24 my-1 rounded-full overflow-clip m-auto"><img src="./dppp.jpg" alt="User Image"/></div>
+<div className="h-24 py-3 flex flex-col justify-end">
+    <button
+        className="center-center py-2 w-full bg-sky-500/100 rounded hover:rounded-full text-xl font-black font-mono text-white hover:text-black hover:bg-sky-500/25">Connect
+    </button>
+</div>
+</div>);
